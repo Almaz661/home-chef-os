77e5c6a73a2c823d44eb23654602b0efc9f0f671
Almaz661 <lutsenkoolga13@gmail.com>
2026-05-17 12:54:21 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-      '/uploads': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
+    port: 5173
+  }
 });
